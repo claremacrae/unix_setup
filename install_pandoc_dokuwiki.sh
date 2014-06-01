fdffef4b5493a5d30219b4d80f0a0dd20e2492ed
claremacrae <github@cfmacrae.fastmail.co.uk>
2014-06-01 20:43:16 +0100

    First stab at current instructions for building my dokuwik additions to pandoc

--- install_pandoc_dokuwiki.sh
+++ install_pandoc_dokuwiki.sh
@@ -18,20 +18,36 @@ echo Setting up dokuwiki version of pandoc code in `pwd` ...
 
 git clone git://github.com/jgm/pandoc-types
 cd pandoc-types
+git checkout bbae35cce0d81bae80e488eaf777537aa487e847
 cabal update
 cabal install --force
 cd ..
 
+# Now clone my copy of pandoc-templates:
+
+git clone https://github.com/claremacrae/pandoc-templates.git
+cd pandoc-templates
+git checkout dokuwiki
+cd ..
+
 # Now install pandoc:
 
-git clone git://github.com/jgm/pandoc
+git clone https://github.com/claremacrae/pandoc.git
 cd pandoc
-git submodule update --init
+git checkout dokuwiki
+#git submodule update --init
+cp ../pandoc-templates/* data/templates/
 cabal install --force --enable-tests
 cabal test
 cd ..
 
 # And finally pandoc-citeproc:
+
+# This was the old version I was using - it is no longer available:
+#git clone git://github.com/jgm/citeproc-hs
+#cd citeproc-hs
+#git checkout 2138963bfac7b5ee41762123395315830081cf2e
+#cd ..
 
 git clone git://github.com/jgm/pandoc-citeproc
 cd pandoc-citeproc
